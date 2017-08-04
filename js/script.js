aa355f38ff7d7c09064dbaedfa91e7f9e6471a97
Jessica Graham <hellojessicagraham@gmail.com>
2017-08-04 19:41:13 +1200

    displays 00 on reset, pause button working

--- js/script.js
+++ js/script.js
@@ -1,59 +1,63 @@
 var startButton = document.getElementById("start");
-var pauseButton = document.getElementById("pause");
-var resetButton = document.getElementById("reset");
-var minutesHTML = document.getElementById("minutes");
-var secondsHTML = document.getElementById("seconds");
-var seconds = 00;
-var minutes = 00;
-var myTimer;
+		pauseButton = document.getElementById("pause"),
+		resetButton = document.getElementById("reset"),
+		minutesHTML = document.getElementById("minutes"),
+		secondsHTML = document.getElementById("seconds"),
+		on = false,
+		seconds = 00,
+		minutes = 00;
 
+var myTimer;
 
 startButton.onclick = startTimer;
 pauseButton.onclick = pauseTimer;
 resetButton.onclick = resetTimer;
 
 function startTimer () {
-	
-	myTimer = setInterval (function () {
-		
-		if (seconds < 10) {
-			secondsHTML.innerHTML = "0" + seconds;
+	if (!on) {
+		myTimer = setInterval (function () {
+
+				seconds ++;
+				on = true;
+
+				if (seconds < 10) {
+					secondsHTML.innerHTML = "0" + seconds;
+				}
+				if (seconds > 9) {
+					secondsHTML.innerHTML = seconds;
+				}
+
+				if (seconds % 60 == 0) {
+					minutes ++;
+					seconds = 00;
+				}
+
+				if (minutes < 10) {
+					minutesHTML.innerHTML = "0" + minutes;
+				}
+
+				if (minutes >= 10) {
+					minutesHTML.innerHTML = minutes;
+				}
+
 		}
-		if (seconds > 9) {
-			secondsHTML.innerHTML = seconds;
-		}
-		
-		seconds ++;
-		
-		if (seconds % 60 == 0) {
-			minutes ++;
-			seconds = 00;
-		}
-		
-		if (minutes < 10) {
-			minutesHTML.innerHTML = "0" + minutes;
-		}
-		
-		if (minutes >= 10) {
-			minutesHTML.innerHTML = minutes;
-		}
-		
-	}		
-	, 1000)
+		, 1000)
+	}
 }
 
 
 function pauseTimer () {
+	on = false;
 	clearInterval(myTimer);
 }
 
 function resetTimer () {
-	seconds = 00;
-	minutes = 00;
-	secondsHTML.innerHTML = seconds;
-	minutesHTML.innerHTML = minutes;
-	
-	console.log(secondsHTML + minutesHTML);
+	on = false;
+	var defaultTime = "00";
+	secondsHTML.innerText = defaultTime;
+	minutesHTML.innerText = defaultTime;
+
+	console.log(myTimer);
 	clearInterval(myTimer);
 
 }
